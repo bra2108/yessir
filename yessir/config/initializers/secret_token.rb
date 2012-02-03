fdb26623024e6f1a7a416b566b24bc188f65fcda
Bayo <bayoadafin@gmail.com>
2012-02-02 21:05:48 -0500

    Initial commit

---
+++ yessir/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yessir::Application.config.secret_token = '017801b71551555471f17630045c39f76593ff345152743f8c2922e4d26796a11788b8b12606c71614d5828ebba6369f94e8af9e539b0565f1ff9bd07aefbc32'
